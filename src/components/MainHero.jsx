539dd83a60b1a2958cf7aedf3fe1ebec2e0e5093
James Nixon <jamesnixon197@gmail.com>
2020-08-29 14:52:46 +0100

    Add new components and change to JSX

---
+++ src/components/MainHero.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class MainHero extends Component {
+    render() {
+        return (
+            <div className="hero">
+            </div>
+        )
+    }
+}

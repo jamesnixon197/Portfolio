f786e8b11cc91e32649507027561447eb136a0ae
James Nixon <jamesnixon197@gmail.com>
2020-08-29 14:57:58 +0100

    Resolve JSX files in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ const devMode = process.env.NODE_ENV !== 'production';
 module.exports = {
     entry: './src/index.js',
     target: 'web',
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
     devServer: {
         inline: false,
         historyApiFallback: true,

7c3306ac91ff15e1d7f1220f74470cf3abbd9e19
James Nixon <jamesnixon197@gmail.com>
2020-08-29 13:27:15 +0100

    Remove prettier and add more linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,25 @@
 module.exports = {
-  env: {
-    node: true,
-    es6: true,
-  },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
     },
-    ecmaVersion: 11,
-    sourceType: "module",
-  },
-  plugins: ["react"],
-  rules: {
-    indent: ["error", 2],
-  },
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 11,
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: {
+        indent: ['error', 4],
+        quotes: ['error', 'single', { avoidEscape: true }],
+        'comma-dangle': ['error', 'always-multiline'],
+    },
 };
